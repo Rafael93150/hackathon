f05fe533639dce3e2e08c59bb71b9db5bf7757ae
Rafael93150 <tavaresrafael93@gmail.com>
2023-07-25 17:23:13 +0200

    add user to login response

--- back/src/models/user.js
+++ back/src/models/user.js
@@ -32,6 +32,10 @@ const userSchema = new mongoose.Schema(
             type: Number,
             default: 0,
         },
+        competences: {
+            type: [String],
+            default: [],
+        },
     },
     {
         timestamps: true,
--- back/src/routes/authRoutes.js
+++ back/src/routes/authRoutes.js
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
 
         const token = jwt.sign(payload, process.env.SECRET_KEY, options);
 
-        res.json({ token });
+        res.json({ user, token });
     } catch (error) {
         res.status(500).json({
             error: `Une erreur est survenue lors de la connexion : ${error}`,
