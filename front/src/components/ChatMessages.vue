4988374bc8a357cadf176b6d91156c8a85693813
Rafael93150 <tavaresrafael93@gmail.com>
2023-07-27 23:21:19 +0200

    starting to add the chat

---
+++ front/src/components/ChatMessages.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chat-message">
+      <span class="chat-message-sender">{{ message.fromUser }}</span>
+      <div class="chat-message-content">{{ message.text }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      message: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .chat-message {
+    background-color: lightgray;
+    padding: 8px;
+    margin-bottom: 8px;
+  }
+  </style>
