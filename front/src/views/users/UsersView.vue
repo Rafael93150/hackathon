7c27f589422596af2fc74bfface7e3b41058e325
Rafael93150 <tavaresrafael93@gmail.com>
2023-07-30 20:47:12 +0200

    fix points text color

--- front/src/views/users/UsersView.vue
+++ front/src/views/users/UsersView.vue
@@ -163,7 +163,9 @@ init();
                   </td>
                   <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                     <div class="text-gray-900">{{ person.title }}</div>
-                    <div class="mt-1 text-gray-500">{{ getCompanyName(person.companies[0]) }}</div>
+                    <div class="mt-1 text-gray-500">
+                      {{ getCompanyName(person.companies[0]) }}
+                    </div>
                   </td>
                   <td
                     class="whitespace-nowrap px-3 py-5 text-sm text-gray-500 text-center"
@@ -175,16 +177,18 @@ init();
                     />
                   </td>
                   <td class="whitespace-nowrap px-3 py-5">
-    <div class="relative h-6 w-full bg-gray-300 rounded-md overflow-hidden">
-      <div
-        class="absolute top-0 left-0 h-full bg-green-400 rounded-md z-neg-1"
-        :style="{ width: `${(person.points / 1000) * 100}%` }"
-      ></div>
-      <div class="text-gray-900 text-center relative z-10">
-        {{ person.points }} / 1000
-      </div>
-    </div>
-  </td>
+                    <div
+                      class="relative h-6 w-full bg-gray-300 rounded-md overflow-hidden"
+                    >
+                      <div
+                        class="absolute top-0 left-0 h-full bg-green-400 rounded-md z-neg-1"
+                        :style="{ width: `${(person.points / 1000) * 100}%` }"
+                      ></div>
+                      <div class="text-gray-600 text-center relative z-10">
+                        {{ person.points }} / 1000
+                      </div>
+                    </div>
+                  </td>
                   <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                     {{
                       person.role === "admin"
